363e477eff15c550a38a6ad9b113156b7e799074
Ferdudas97 <ferdudas97@gmail.com>
2018-11-22 20:27:18 +0100

    Initial commit

---
+++ src/main/java/com/miss/artificial_city/ArtificialCityApplication.java
@@ -0,0 +1,12 @@
+package com.miss.artificial_city;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtificialCityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtificialCityApplication.class, args);
+    }
+}
